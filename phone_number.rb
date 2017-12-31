02d09efe104a88babab1f62015aecbce3f4449c9
Mark R <mirubenstein@gmail.com>
2017-12-31 12:53:57 -0800

    Add regex solution from @sutamahman

--- phone_number.rb
+++ phone_number.rb
@@ -1,61 +1,8 @@
-class PhoneCode
-  attr_reader :code
-
-  def initialize(code)
-    @code = code
-  end
-
-  def valid?
-    code.first.to_i.between? 2, 9
-  end
-end
-
-class InternationalCountry < PhoneCode
-  attr_reader :valid
-
-  def initialize(code)
-    @code = []
-    @valid = code.first .to_i == 1
-  end
-
-  def valid?
-    valid
-  end
-end
-
-class Area < PhoneCode
-end
-
-class Local < PhoneCode
-end
-
 class PhoneNumber
   def self.clean(phone_number)
-    numbers = phone_number.scan(/\d/)
-    phone_number_parts = case numbers.length
-      when 10
-        [
-          Area.new(numbers.slice(0..2)),
-          Local.new(numbers.slice(3..10))
-        ]
-      when 11
-        [
-          InternationalCountry.new(numbers.slice(0...1)),
-          Area.new(numbers.slice(1..3)),
-          Local.new(numbers.slice(4..11))
-        ]
-      else
-        []
+    phone_number.match(/^\+?1?\D*([2-9]\d{2})\D*([2-9]\d{2})\D*(\d{4})\D*$/) do |x|
+      "#{x[1]}#{x[2]}#{x[3]}"
     end
-    if valid? phone_number_parts
-      phone_number_parts.flat_map(&:code).join
-    else
-      nil
-    end
-  end
-
-  def self.valid?(phone_number_parts)
-    phone_number_parts.all?(&:valid?) && !phone_number_parts.empty?
   end
 end
 
